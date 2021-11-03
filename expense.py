9f7cd4ee6ef4ff369b6e43a201479d64faceb145
maxime.colomes <maxime.colomes@epita.fr>
2021-11-03 20:01:11 +0100

    Added expense registry and test management

--- expense.py
+++ expense.py
@@ -1,4 +1,6 @@
 from PyInquirer import prompt
+import csv
+
 
 expense_questions = [
     {
@@ -22,9 +24,23 @@ expense_questions = [
 
 
 def new_expense(*args):
-    infos = prompt(expense_questions)
-    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    if (args):
+        infos = {'amount': args[0], 'label': args[1], 'spender': args[2]}
+    else:
+        infos = prompt(expense_questions)
+
+    # Error management
+    if (type(infos.get('amount')) != int):
+        print("Error. The amount must be a number !")
+        return False
+    if (type(infos.get('spender')) != str):
+        print("Error. The spender must be a string !")
+        return False
+
+
+    csvfile = open('expense_report.csv', 'a', newline='')
+    expensewriter = csv.writer(csvfile, delimiter=' '
+                        , quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    expensewriter.writerow([infos.get('amount'), infos.get('label'), infos.get('spender')])
     print("Expense Added !")
     return True
-
-
--- tests/expense.py
+++ tests/expense.py
@@ -5,4 +5,5 @@ class TestExpense(unittest.TestCase):
 
     def test_add_expense(self):
         self.assertEqual(new_expense(23, 'toto', 'pchojka'), True)
-        self.assertEqual(new_expense('bonjour',23,23), False)
+        self.assertEqual(new_expense('bonjour',23,'pchojka'), False) # Check that amount is an int
+        self.assertEqual(new_expense(20, 23, 23), False) # Check that name is a str
