1decf23ef464e5192faf076f0bc6cb45fef6abac
maxime.colomes <maxime.colomes@epita.fr>
2021-11-03 22:06:11 +0100

    finished report

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PyInquirer import prompt
 from examples import custom_style_2
-from expense import expense_questions,new_expense
+from expense import new_expense
+from report import show_report
 from user import add_user
 
 def ask_option():
@@ -16,6 +17,9 @@ def ask_option():
         ask_option()
     if (option['main_options']) == "New User":
         add_user()
+        ask_option()
+    if (option['main_options']) == "Show Status":
+        show_report()
         ask_option()
 
 def main():
